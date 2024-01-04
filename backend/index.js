c0096c4be6071a930d6a4a8d8c1921c61aef892a
Nestor Rosales <rosales.nestorr@gmail.com>
2024-01-04 20:51:11 -0300

    header update

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use((req, res, next) => {
 	res.header(
 		"Access-Control-Allow-Origin",
-		"https://rosalesnestor-dev.vercel.app"
+		"https://rosalesnestor-dev.vercel.app/"
 	);
 	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
 	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
